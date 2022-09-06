64508f572d11c51e52012554caf345fa51b1f01e
craig cardwell <wizeduvcc@gmail.com>
2022-09-06 09:08:41 -0600

    update pr and merge

--- app/models/Applicant.rb
+++ app/models/Applicant.rb
@@ -1,5 +1,5 @@
 class Applicant < ApplicationRecord
-  has_many :job_applicants
+  has_many :job_applicants, dependent: :destroy
   has_many :jobs, through: :job_applicants
   has_many :companies, through: :jobs
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ApplicationRecord
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
   has_many :job_applicants, through: :jobs
   has_many :applicants, through: :job_applicants
 end
